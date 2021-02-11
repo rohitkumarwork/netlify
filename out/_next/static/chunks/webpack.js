17f239008eabb9730963ee7b09590e444d399dc0
rohitkumarwork <rohitkumar1work@gmail.com>
2021-02-11 18:29:17 +0530

    create build

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4fee97db266369a517d4";
+/******/ 	var hotCurrentHash = "a0053ee58ccb1e902ff2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
