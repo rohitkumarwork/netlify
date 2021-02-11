60f664b7d53bc2bc615dcc8940f0543ba8c9c2da
rohitkumarwork <rohitkumar1work@gmail.com>
2021-02-11 19:39:28 +0530

    update sidebar

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a0053ee58ccb1e902ff2";
+/******/ 	var hotCurrentHash = "2b89a7b2dd72a118ab22";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
